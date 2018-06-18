f3508b12855abae7947e88c4a616dbd5a2cdb090
Pratik Raj <Rajpratik71@gmail.com>
2018-06-18 17:15:17 +0530

    Signed-off-by: Pratik Raj <Rajpratik71@gmail.com>

--- ifelseif.py
+++ ifelseif.py
@@ -1,5 +1,7 @@
 a = int(input('Enter the no :' ))
-if  a > 5 :
-    print("chota no dal")
-elif a == 0
+if  a == 0 :
     print("Please Enter non zero ")
+elif a > 5 :
+    print("Please Enter less than 5")
+elif a < 5:
+    print("Please Enter greater than 5")
---
+++ marks.py
@@ -0,0 +1,7 @@
+marks = int(input("enter your marks"))
+if marks >= 90:
+    print("your grade is A")
+elif marks >=75:
+    print("your grade is B")
+elif marks >=60:
+    print("your grade is C")
